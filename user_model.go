737a35fc7291ba38963c9d13948a5e0b187c0489
Teerasej <teerasej@gmail.com>
2021-04-02 22:32:11 +0700

    กำหนด DefaultModel จะเป็นการเพิ่ม field _id, created_at และ updated_at เข้ามาใน model

--- user_model.go
+++ user_model.go
@@ -1,7 +1,12 @@
 package main
 
+import (
+	"github.com/kamva/mgm/v3"
+)
+
 type UserData struct {
-	Userid   int    `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	mgm.DefaultModel `bson:",inline"`
+	Userid           int    `json:"id"`
+	Email            string `json:"email" bson:"email"`
+	Password         string `json:"password" bson:"password"`
 }
