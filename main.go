0ae16ebabcd4d2ac92544b321704ffa1fe428fe2
Teerasej <teerasej@gmail.com>
2021-04-02 19:13:17 +0700

    สร้าง route สำหรับคืนค่า user กำหนดสร้าง User Data ใช้ json.NewEncoder เพื่อแปลง struct เป็น json

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("ok")
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	userResponse := UserData{
+		Userid:   28566777,
+		Email:    "training@nextflow.in.th",
+		Password: "1234",
+	}
+	json.NewEncoder(w).Encode(userResponse)
 }
